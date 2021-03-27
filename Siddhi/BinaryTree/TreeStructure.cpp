714e55e526f52aad239f2f55a9169b63ae1eeca9
SiddhiVTripathi <siddhi19vinayak@gmail.com>
2021-03-27 11:47:53 +0530

    Basic structure code of binary tree

---
+++ Siddhi/BinaryTree/TreeStructure.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+ 
+    // val is the key or the value that
+    // has to be added to the data part
+    Node(int val)
+    {
+        data = val;
+ 
+        // Left and right child for node
+        // will be initialized to null
+        left = NULL;
+        right = NULL;
+    }
+};
