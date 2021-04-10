4d98fc034763274309e42b220534ca06c954efcd
SiddhiVTripathi <siddhi19vinayak@gmail.com>
2021-04-10 13:37:12 +0530

    Name change

---
+++ Siddhi/Tree/invertBinaryTree.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+ 
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+ 
+    // val is the key or the value that
+    // has to be added to the data part
+    Node(int val)
+    {
+        data = val;
+ 
+        // Left and right child for node
+        // will be initialized to null
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void invert(struct Node* root)
+{
+    if(root == NULL)
+        return;
+    
+    invert(root->left);
+    invert(root->right);
+    struct Node *temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+}
